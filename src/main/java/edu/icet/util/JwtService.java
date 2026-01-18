ca3b78ce3a4da2dd2f6fa729753fa523fd4b8d78
Pramudithalakshan <pramudithalakshan00@gmail.com>
2026-01-18 21:15:16 +0530

    Fix - Remove final key word from secretKey constant Lombok try to initialize that variable in the initial state. So lombok expect String type Bean

--- src/main/java/edu/icet/util/JwtService.java
+++ src/main/java/edu/icet/util/JwtService.java
@@ -4,7 +4,6 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -16,10 +15,9 @@ import java.util.Map;
 import java.util.function.Function;
 
 @Service
-@RequiredArgsConstructor
 public class JwtService {
     @Value("${SECRET_KEY}")
-    private final String secretKey;
+    private String secretKey;
     public String generateToken(String username){
         Map<String,Object> claims = new HashMap<>();
         return createToken(claims,username);
